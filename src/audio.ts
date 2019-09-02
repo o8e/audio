63acac72c92e12ab75424b8ddedc4f0dd57e27e1
Ollie Tribe <tribe@myself.com>
2019-09-02 10:52:07 +0100

    Update test path

--- src/audio.ts
+++ src/audio.ts
@@ -8,13 +8,13 @@ const readdir = util.promisify(fs.readdir)
 
 export const format = async (path: string) => {
   const files = await readdir(path)
-  for (const file of files) {
+  files.forEach(file => {
     if (isMP3(file)) {
-      const [artist, title] = file.split(' - ')
+      const [artist, title] = file.replace('.mp3', '').split(' - ')
       const tags = { title, artist }
       return id3.write(tags, `${path}/${file}`)
     }
-  }
+  })
 }
 
 const isMP3 = (filename: string) => last(filename.split('.')) === 'mp3'
